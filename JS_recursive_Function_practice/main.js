57714d564bb0cd51645dd018d55b303706bebc63
shree0007 <shree.bhusal07@gmail.com>
2024-01-04 13:19:15 +0200

    JS recursive function practice

---
+++ JS_recursive_Function_practice/main.js
@@ -0,0 +1,10 @@
+function rev(num) {
+    if (num === 0) {
+        return 1
+    } else {
+        let y = num * rev(num - 1)
+        console.log(num, y)
+        return y
+    }
+}
+console.log(rev(5));
